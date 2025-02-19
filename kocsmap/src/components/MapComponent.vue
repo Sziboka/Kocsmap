a602a9bfcffba558c9bd0e51faeb68391ae313f0
bszanto <botondszanto450@gmail.com>
2025-02-19 17:55:38 +0100

    creating vue app base

---
+++ kocsmap/src/components/MapComponent.vue
@@ -0,0 +1,20 @@
+<!-- filepath: /kocsmap-vue/src/components/MapComponent.vue -->
+<template>
+    <div id="map"></div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MapComponent',
+    mounted() {
+      // Your map initialization code here
+    }
+  }
+  </script>
+  
+  <style scoped>
+  #map {
+    width: 100%;
+    height: 100vh;
+  }
+  </style>
